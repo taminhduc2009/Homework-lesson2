ea7b3cd82803c3bff6013c97f05a14a7daf1a83f
taminhduc2009 <bluebest0912@gmail>
2023-07-10 09:39:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//Bài 1+2
+
+let fruit1 =[["grape",5],["apple",2]]
+for(let i=0;i<fruit1.length;i++){
+    console.log(i)
+    let key = fruit1[i][0]
+    let value = fruit1[i][1]
+    console.log(key,":",value)
+
+}
+
+
+//Bài 3
+var Husky = {
+    "name": "Ngáo",
+    "legs": 4,
+    "color": "Brown",
+};
+
